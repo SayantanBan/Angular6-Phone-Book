715f9967748bbd5c3fe585fb7352f32549cf4702
Sayantan Banerjee <sayantanbanerjee@outlook.com>
2019-04-25 02:49:21 +0530

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO contact_details(id, first_name, last_name, email, phone, group_by)
+VALUES
+       (100001, 'John', 'Doe', 'john.doe@xyz.com', 1234567890, 'Family'),
+       (100002, 'Bela', 'Bose', 'bela.bose@xyz.com', 1234567890, 'Family'),
+       (100003, 'Arjun', 'Kapoor', 'arjun.kapoor@xyz.com', 1234567890, 'Friend'),
+	   (100004, 'Varun', 'Dhawan', 'varun.dhawan@xyz.com', 1234567890, 'Friend'),
+	   (100005, 'Sayantan', 'Banerjee', 'sayantan.banerjee@xyz.com', 1234567890, 'Friend'),
+       (100006, 'Akshay', 'Kumar', 'akshay.kumar@xyz.com', 1234567890, 'Friend');
